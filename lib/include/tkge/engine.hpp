cd7f7780404f042d925c6856edc420e4dabce761
Tymianek <tymi@tymi.org>
2025-02-15 12:46:47 +0100

    More fix (#11)
    * more clang format
    
    * Asset loader
    
    * Update kvf (track render command buffer recording state) (#5)
    
    * PascalCase functions (#6)
    
    * Add `graphics::Shader`, draw triangle with temp code (#7)
    
    * Add `graphics::Shader`, draw triangle with temp code
    
    * Fix warnings in Release builds
    
    * Guard against invalid SPIR-V
    
    * Update clang format
    
    * remove stupid annoying spaces
    
    * remove unnecessary cmake file ref
    
    ---------
    
    Co-authored-by: Karn Kaul <karnkaul@gmail.com>

--- lib/include/tkge/engine.hpp
+++ lib/include/tkge/engine.hpp
@@ -5,38 +5,48 @@
 #include <kvf/render_device.hpp>
 #include <kvf/render_pass.hpp>
 #include <kvf/window.hpp>
+#include "assetLoader.hpp"
 
 namespace tkge
 {
-struct WindowSurface
-{
-	glm::ivec2 size{600};
-	klib::CString title{"tkge"};
-	bool decorated{true};
-};
+	struct WindowSurface
+	{
+		glm::ivec2 size{600};
+		klib::CString title{"tkge"};
+		bool decorated{true};
+	};
 
-class Engine
-{
-  public:
-	static constexpr auto AntiAliasing = vk::SampleCountFlagBits::e2;
+	class Engine
+	{
+	  public:
+		static constexpr auto AntiAliasing = vk::SampleCountFlagBits::e2;
 
-	explicit Engine(WindowSurface const& surface = {}, vk::SampleCountFlagBits aa = AntiAliasing);
+		explicit Engine(const WindowSurface& surface = {}, vk::SampleCountFlagBits aa = AntiAliasing);
 
-	[[nodiscard]] glm::ivec2 FramebufferSize() const;
-	[[nodiscard]] bool IsRunning() const;
+		[[nodiscard]] const kvf::RenderDevice& RenderDevice() const { return _renderDevice; }
+		[[nodiscard]] const kvf::RenderPass& RenderPass() const { return _renderPass; }
 
-	vk::CommandBuffer NextFrame();
-	// TODO: return Renderer
-	void BeginRender(kvf::Color clear = kvf::black_v);
-	void EndRender();
+		[[nodiscard]] glm::ivec2 FramebufferSize() const;
+		[[nodiscard]] auto FramebufferFormat() const -> vk::Format { return _renderPass.get_color_format(); }
+		[[nodiscard]] auto FramebufferSamples() const -> vk::SampleCountFlagBits { return _renderPass.get_samples(); }
 
-  private:
-	[[nodiscard]] kvf::UniqueWindow CreateWindow(WindowSurface const& surface);
+		[[nodiscard]] bool IsRunning() const;
+		vk::CommandBuffer NextFrame();
+		// TODO: return Renderer
+		void BeginRender(kvf::Color clear = kvf::black_v);
+		void EndRender();
 
-	kvf::UniqueWindow _window;
-	kvf::RenderDevice _renderDevice;
-	kvf::RenderPass _renderPass;
+		[[nodiscard]] AssetLoader& GetAssetLoader() noexcept { return this->_assetLoader; }
+		[[nodiscard]] const AssetLoader& GetAssetLoader() const noexcept { return this->_assetLoader; }
 
-	vk::CommandBuffer _cmd{};
-};
+	  private:
+		[[nodiscard]] kvf::UniqueWindow CreateWindow(const WindowSurface& surface);
+
+		kvf::UniqueWindow _window;
+		kvf::RenderDevice _renderDevice;
+		kvf::RenderPass _renderPass;
+
+		vk::CommandBuffer _cmd{};
+		AssetLoader _assetLoader;
+	};
 } // namespace tkge
