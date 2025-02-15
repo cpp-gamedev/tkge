332780dd53bb959aad185eaad7512c9b94178b06
TymianekPL <tymi@tymi.org>
2025-02-15 13:18:12 +0100

    Fix formatting

--- lib/include/tkge/Assets/TextAsset.hpp
+++ lib/include/tkge/Assets/TextAsset.hpp
@@ -11,11 +11,12 @@ namespace tkge::Assets
 		void Load(ReadonlyByteStream byteStream) override
 		{
 			const auto vData = byteStream.AsSpan<char>(0, byteStream.GetStreamSize());
-			this->_text = std::string{ vData.begin(), vData.end() };
+			this->_text = std::string{vData.begin(), vData.end()};
 		}
 
 		[[nodiscard]] const std::string& text() const noexcept { return this->_text; }
-	private:
+
+	  private:
 		std::string _text;
 	};
 } // namespace tkge::Assets
