e68bfa3d487a5e73d2218a1d9ee873dcd75dc52f
Karn Kaul <karnkaul@gmail.com>
2025-02-15 00:05:42 -0800

    Add `graphics::Shader`, draw triangle with temp code (#7)
    * Add `graphics::Shader`, draw triangle with temp code
    
    * Fix warnings in Release builds
    
    * Guard against invalid SPIR-V
    
    * Update clang format

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,31 +1,95 @@
 #include <imgui.h>
+#include <klib/assert.hpp>
 #include <tkge/engine.hpp>
 #include <exception>
 #include <print>
 
+// temporary, until we have shader assets.
+#include <tkge/graphics/shader.hpp>
+#include <filesystem>
+#include <fstream>
+#include <vector>
+
 namespace
 {
-void Run()
-{
-	static constexpr tkge::WindowSurface surface{.size = {1280, 720}};
-	tkge::Engine engine{surface};
-	while (engine.IsRunning())
+	namespace fs = std::filesystem;
+
+	fs::path Upfind(const fs::path& leafDir, const std::string_view pattern)
 	{
-		engine.NextFrame();
-		ImGui::ShowDemoWindow();
-		engine.BeginRender();
-		engine.EndRender();
+		for (auto path = leafDir; path.has_parent_path() && path.parent_path() != path; path = path.parent_path())
+		{
+			auto ret = path / pattern;
+			if (fs::exists(ret)) { return fs::absolute(ret); }
+		}
+		return fs::current_path();
 	}
-}
+
+	std::vector<std::uint32_t> LoadSpirV(const klib::CString path)
+	{
+		auto file = std::ifstream{path.c_str(), std::ios::binary | std::ios::ate};
+		if (!file) { return {}; }
+
+		const auto size = file.tellg();
+		if (std::size_t(size) % sizeof(std::uint32_t) != 0) { return {}; } // invalid SPIR-V.
+
+		file.seekg(std::ios::beg, {});
+		auto ret = std::vector<std::uint32_t>{};
+		ret.resize(std::size_t(size) / sizeof(std::uint32_t));
+		file.read(reinterpret_cast<char*>(ret.data()), size); // NOLINT(cppcoreguidelines-pro-type-reinterpret-cast)
+		return ret;
+	}
+
+	void Run(const fs::path& assets_path)
+	{
+		static constexpr tkge::WindowSurface surface{.size = {1280, 720}};
+		tkge::Engine engine{surface};
+
+		auto shader = tkge::graphics::Shader{};
+		const auto vertexSpirV = LoadSpirV((assets_path / "shaders/triangle.vert").string().c_str());
+		const auto fragmentSpirV = LoadSpirV((assets_path / "shaders/triangle.frag").string().c_str());
+		const auto& renderDevice = engine.RenderDevice();
+		if (!shader.Load(renderDevice.get_device(), vertexSpirV, fragmentSpirV)) { throw std::runtime_error{"Failed to load shaders"}; }
+
+		const auto pipelineLayout = renderDevice.get_device().createPipelineLayoutUnique({});
+
+		const auto pipelineState = kvf::PipelineState{
+			.vertex_bindings = {},
+			.vertex_attributes = {},
+			.vertex_shader = shader.VertexModule(),
+			.fragment_shader = shader.FragmentModule(),
+		};
+		const auto pipelineFormat = kvf::PipelineFormat{
+			.samples = engine.FramebufferSamples(),
+			.color = engine.FramebufferFormat(),
+		};
+		const auto pipeline = renderDevice.create_pipeline(*pipelineLayout, pipelineState, pipelineFormat);
+		if (!pipeline) { throw std::runtime_error{"Failed to create graphics pipeline"}; }
+
+		while (engine.IsRunning())
+		{
+			const auto commandBuffer = engine.NextFrame();
+
+			ImGui::ShowDemoWindow();
+
+			engine.BeginRender();
+			engine.RenderPass().bind_pipeline(*pipeline);
+			commandBuffer.draw(3, 1, 0, 0);
+			engine.EndRender();
+		}
+
+		renderDevice.get_device().waitIdle();
+	}
 } // namespace
 
-int main()
+int main([[maybe_unused]] int argc, char** argv)
 {
 	try
 	{
-		Run();
+		KLIB_ASSERT(argc > 0);
+		const auto assets_path = Upfind(*argv, "assets");
+		Run(assets_path);
 	}
-	catch (std::exception const& e)
+	catch (const std::exception& e)
 	{
 		std::println(stderr, "PANIC: {}", e.what());
 		return EXIT_FAILURE;
--- lib/src/engine.cpp
+++ lib/src/engine.cpp
@@ -4,40 +4,41 @@
 
 namespace tkge
 {
-Engine::Engine(WindowSurface const& surface, vk::SampleCountFlagBits const aa)
-	: _window(CreateWindow(surface)), _renderDevice(_window.get()), _renderPass(&_renderDevice, aa)
-{
-}
+	Engine::Engine(const WindowSurface& surface, const vk::SampleCountFlagBits aa)
+		: _window(CreateWindow(surface)), _renderDevice(_window.get()), _renderPass(&_renderDevice, aa)
+	{
+		_renderPass.set_color_target();
+	}
 
-glm::ivec2 Engine::FramebufferSize() const { return kvf::util::to_glm_vec<int>(_renderDevice.get_framebuffer_extent()); }
+	glm::ivec2 Engine::FramebufferSize() const { return kvf::util::to_glm_vec<int>(_renderDevice.get_framebuffer_extent()); }
 
-bool Engine::IsRunning() const { return glfwWindowShouldClose(_window.get()) == GLFW_FALSE; }
+	bool Engine::IsRunning() const { return glfwWindowShouldClose(_window.get()) == GLFW_FALSE; }
 
-vk::CommandBuffer Engine::NextFrame()
-{
-	_cmd = _renderDevice.next_frame();
-	return _cmd;
-}
+	vk::CommandBuffer Engine::NextFrame()
+	{
+		_cmd = _renderDevice.next_frame();
+		return _cmd;
+	}
 
-kvf::UniqueWindow Engine::CreateWindow(WindowSurface const& surface)
-{
-	auto ret = kvf::create_window(surface.size, surface.title, surface.decorated);
-	KLIB_ASSERT(ret);
-	glfwSetWindowUserPointer(ret.get(), this);
-	// TODO
-	// setGlfwCallbacks(ret.get());
-	return ret;
-}
+	kvf::UniqueWindow Engine::CreateWindow(const WindowSurface& surface)
+	{
+		auto ret = kvf::create_window(surface.size, surface.title, surface.decorated);
+		KLIB_ASSERT(ret);
+		glfwSetWindowUserPointer(ret.get(), this);
+		// TODO
+		// setGlfwCallbacks(ret.get());
+		return ret;
+	}
 
-void Engine::BeginRender(kvf::Color const clear)
-{
-	_renderPass.clear_color = clear.to_linear();
-	_renderPass.begin_render(_cmd, _renderDevice.get_framebuffer_extent());
-}
+	void Engine::BeginRender(const kvf::Color clear)
+	{
+		_renderPass.clear_color = clear.to_linear();
+		_renderPass.begin_render(_cmd, _renderDevice.get_framebuffer_extent());
+	}
 
-void Engine::EndRender()
-{
-	_renderPass.end_render();
-	_renderDevice.render(_renderPass.render_target());
-}
+	void Engine::EndRender()
+	{
+		_renderPass.end_render();
+		_renderDevice.render(_renderPass.render_target());
+	}
 } // namespace tkge
