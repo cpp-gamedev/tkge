3099f316cca3d626ac2908bbe9574e8365ffb30d
TymianekPL <tymi@tymi.org>
2025-02-16 09:46:23 +0100

    Asset loader fix

--- lib/include/tkge/assetLoader.hpp
+++ lib/include/tkge/assetLoader.hpp
@@ -10,13 +10,18 @@ namespace tkge
 	class AssetLoader
 	{
 	  public:
+		explicit AssetLoader() : _paths(this->GetSearchPaths()) {}
+		AssetLoader(const AssetLoader&) = delete;
+		AssetLoader(AssetLoader&&) = default;
+		AssetLoader& operator=(const AssetLoader&) = delete;
+		AssetLoader& operator=(AssetLoader&&) = default;
+		~AssetLoader() = default;
+
 		template <typename T>
 			requires std::is_base_of_v<Assets::IAsset, T>
 		std::unique_ptr<T> LoadAsset(const std::string& fileName) const
 		{
-			const static std::vector<std::filesystem::path> paths = this->GetSearchPaths();
-
-			for (const auto& path : paths)
+			for (const auto& path : this->_paths)
 			{
 				if (std::filesystem::exists(path / fileName))
 				{
@@ -30,5 +35,8 @@ namespace tkge
 		}
 
 		[[nodiscard]] std::vector<std::filesystem::path> GetSearchPaths() const;
+
+	  private:
+		std::vector<std::filesystem::path> _paths;
 	};
 } // namespace tkge
