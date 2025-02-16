6cd589a89d61e2b0f78a8abe87208fe58c993e19
TymianekPL <tymi@tymi.org>
2025-02-16 09:36:01 +0100

    formating

--- lib/src/Assets/TextAsset.cpp
+++ lib/src/Assets/TextAsset.cpp
@@ -3,6 +3,6 @@
 bool tkge::Assets::TextAsset::Load(ReadonlyByteStream byteStream)
 {
 	const auto vData = byteStream.AsSpan<char>(0, byteStream.GetStreamSize());
-	this->_text = std::string{ vData.begin(), vData.end() };
+	this->_text = std::string{vData.begin(), vData.end()};
 	return true;
 }
